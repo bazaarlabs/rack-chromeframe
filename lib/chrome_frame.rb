8d7c41edd4f4ac69f53b4ebba7cbf4c4024c39de
Nico Taing <nico.taing@gmail.com>
2012-02-17 15:32:42 -0800

    fix checking header content-type

--- lib/chrome_frame.rb
+++ lib/chrome_frame.rb
@@ -7,21 +7,21 @@ module Rack
 
     def call(env)
       status, headers, response = @app.call(env)
-      if env['HTTP_USER_AGENT'] =~ /MSIE/ && response.content_type == 'text/html'
-        new_body = insert_goods(build_response_body(response)) 
+      if env['HTTP_USER_AGENT'] =~ /MSIE/ && headers["Content-Type"] =~ /text\/html/i
+        new_body = insert_goods(build_response_body(response))
         new_headers = recalculate_body_length(headers, new_body)
         [status, new_headers, new_body]
       else
         [status, headers, response]
       end
     end
-    
+
     def build_response_body(response)
       response_body = ""
       response.each { |part| response_body += part }
       response_body
     end
-    
+
     def recalculate_body_length(headers, body)
       new_headers = headers
       new_headers["Content-Length"] = body.length.to_s
@@ -32,7 +32,7 @@ module Rack
       head = <<-HEAD
       <meta http-equiv="X-UA-Compatible" content="chrome=1">
       HEAD
-      
+
       bod = <<-BOD
       <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/chrome-frame/1/CFInstall.min.js"></script>
       <div id="cf-placeholder"></div>
